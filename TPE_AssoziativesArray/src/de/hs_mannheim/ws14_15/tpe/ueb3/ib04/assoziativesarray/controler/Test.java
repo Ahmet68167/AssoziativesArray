f6c890b862a6fdd81daaddf2e81c2109fc09ea75
Ahmet <Ahmet@192.168.1.50>
2014-11-25 21:10:23 +0100

    test test

--- TPE_AssoziativesArray/src/de/hs_mannheim/ws14_15/tpe/ueb3/ib04/assoziativesarray/controler/Test.java
+++ TPE_AssoziativesArray/src/de/hs_mannheim/ws14_15/tpe/ueb3/ib04/assoziativesarray/controler/Test.java
@@ -1,30 +1,34 @@
 package de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.controler;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model.binaerbaum.BinaerBaum;
+import de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model.binaerbaum.BinearBaumAssoziativesArray;
 import de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model.binaerbaum.Knoten;
 
 public class Test {
 
 	public static void main(String[] args) {
-		BinaerBaum<Integer, String> a = new BinaerBaum<>();
+		BinearBaumAssoziativesArray<Integer, String> a = new BinearBaumAssoziativesArray<>();
+		BinearBaumAssoziativesArray<Integer, String> b = new BinearBaumAssoziativesArray<>();
 		
-		a.hinzufuegen(30, "A");
-		a.hinzufuegen(20, "B");
-		a.hinzufuegen(10, "C");
-		a.hinzufuegen(50, "D");
-		a.hinzufuegen(70, "E");
-		a.hinzufuegen(60, "F");
-		a.hinzufuegen(90, "G");
-		a.hinzufuegen(40, "K");
-		a.hinzufuegen(35, "O");
+		a.put(30, "A");
+		a.put(20, "B");
+		a.put(10, "C");
+		a.put(50, "D");
+		a.put(70, "E");
+		a.put(60, "F");
+		a.put(90, "G");
+		a.put(40, "K");
+		a.put(35, "O");
 		
-		System.out.println(a.toString());
+		System.out.println(a);
+
+		b.put(21, "KI");
+		b.put(42, "AL");
+		b.put(54, "OP");
+		b.put(86, "OI");
 		
-		a.setWurzel(null);
-		System.out.println(a.toString());
+		System.out.println(a.remove(900));
+		
+		System.out.println(a);
 		
 	}
 	

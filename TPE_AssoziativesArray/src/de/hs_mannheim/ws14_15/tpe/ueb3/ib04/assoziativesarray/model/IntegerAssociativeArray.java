92bfbc838847f23fed497c1439f5c0604e56e688
Ahmet <Ahmet@Ahmet-PC.localdomain>
2014-11-26 12:08:30 +0100

    IntegerAssociativeArray fertig

--- TPE_AssoziativesArray/src/de/hs_mannheim/ws14_15/tpe/ueb3/ib04/assoziativesarray/model/IntegerAssociativeArray.java
+++ TPE_AssoziativesArray/src/de/hs_mannheim/ws14_15/tpe/ueb3/ib04/assoziativesarray/model/IntegerAssociativeArray.java
@@ -2,8 +2,19 @@ package de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model;
 
 import de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model.binaerbaum.BinearBaumAssoziativesArray;
 
+/**
+ * Erstellt ein AssociativeArray, dass nur Integer als Wert annimmt
+ * 
+ * @author Ahmet
+ *
+ * @param <S> Datentyp des Schluessels
+ */
 public class IntegerAssociativeArray<S> extends BinearBaumAssoziativesArray<S, Integer> {
 
+	/**
+	 * legt ein IntegerAssociativeArray an
+	 * 
+	 */
 	public IntegerAssociativeArray() {
 		
 	}

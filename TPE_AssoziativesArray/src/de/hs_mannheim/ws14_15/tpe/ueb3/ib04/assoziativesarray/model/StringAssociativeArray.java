013ff738fa8aa1020b64be0c2554a03b7989a765
Ahmet <Ahmet@Ahmet-PC.localdomain>
2014-11-26 12:10:15 +0100

    StringAssociativeArray fertig

--- TPE_AssoziativesArray/src/de/hs_mannheim/ws14_15/tpe/ueb3/ib04/assoziativesarray/model/StringAssociativeArray.java
+++ TPE_AssoziativesArray/src/de/hs_mannheim/ws14_15/tpe/ueb3/ib04/assoziativesarray/model/StringAssociativeArray.java
@@ -2,8 +2,19 @@ package de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model;
 
 import de.hs_mannheim.ws14_15.tpe.ueb3.ib04.assoziativesarray.model.binaerbaum.BinearBaumAssoziativesArray;
 
+/**
+ * Erstellt ein AssociativeArray an, das nur String als Schluessel annimmt
+ * 
+ * @author Ahmet
+ *
+ * @param <W> Datentyp der Werte
+ */
 public class StringAssociativeArray<W> extends BinearBaumAssoziativesArray<String, W> {
 	
+	/**
+	 * Legt ein StringAssociativeArray an
+	 * 
+	 */
 	public StringAssociativeArray() {
 		
 	}
